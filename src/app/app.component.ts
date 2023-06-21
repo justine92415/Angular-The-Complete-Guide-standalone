6b3d7bbb03d881de7948ce4bcfe685563c68d8dd
justine92415 <justine92415@gmail.com>
2023-06-21 22:19:07 +0800

    建立各個元件布局

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,RecipesComponent,ShoppingListComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
