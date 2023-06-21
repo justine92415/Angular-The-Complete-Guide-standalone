df0b18fada0358b4c9180e49c7154be16ca244a9
justine92415 <justine92415@gmail.com>
2023-06-21 22:42:08 +0800

    加入 recipe-list 布局

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Recipe } from '../../recipe.model';
 
 @Component({
   selector: 'app-recipe-item',
@@ -9,7 +10,9 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-  recipes = [];
+  recipes:Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://www.maxpixel.net/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg'),
+  ];
   
   constructor() {}
 
