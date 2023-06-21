35e0646ec01fadbe072f76c91f196ab28c2d7461
justine92415 <justine92415@gmail.com>
2023-06-21 23:00:15 +0800

    增加 ingredient.model、使用 ngFor顯示配方清單

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   standalone: true,
-  imports: [CommonModule,ShoppingEditComponent],
+  imports: [CommonModule, ShoppingEditComponent],
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
-
+  ingredients:Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
 }
