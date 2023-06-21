ba92f85ea341f203b6cc88840f9fb42a9e302232
justine92415 <justine92415@gmail.com>
2023-06-21 22:25:09 +0800

    加入 Navigation Bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,RecipesComponent,ShoppingListComponent],
+  imports: [CommonModule, RouterOutlet,HeaderComponent,RecipesComponent,ShoppingListComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
