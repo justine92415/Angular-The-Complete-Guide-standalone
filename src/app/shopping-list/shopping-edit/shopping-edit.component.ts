39121940bf95a2689f0f045faaf19d5365f99ba5
justine92415 <justine92415@gmail.com>
2023-06-22 15:35:30 +0800

    新增 加入 Ingredient 功能

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-edit',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './shopping-edit.component.html',
-  styleUrls: ['./shopping-edit.component.css']
+  styleUrls: ['./shopping-edit.component.css'],
 })
 export class ShoppingEditComponent {
+  @ViewChild('nameInput', { static: false }) nameInputRef!: ElementRef;
+  @ViewChild('amountInput', { static: false }) amountInputRef!: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
+  onAddItem(){
+    const ingName = this.nameInputRef.nativeElement.value;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName,ingAmount);
+    this.ingredientAdded.emit(newIngredient);
+  };
 }
