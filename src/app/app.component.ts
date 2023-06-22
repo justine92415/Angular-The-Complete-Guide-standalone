f09e2b8e979663405a9a8b7fc70a435b93c9133d
justine92415 <justine92415@gmail.com>
2023-06-22 13:50:01 +0800

    增加 cmp-databinding 分支

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,39 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
-import { RecipesComponent } from './recipes/recipes.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { HeaderComponent } from './header/header.component';
+import { Component } from '@angular/core';
+import { CockpitComponent } from './cockpit/cockpit.component';
+import { ServerElementComponent } from './server-element/server-element.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,HeaderComponent,RecipesComponent,ShoppingListComponent],
+  imports: [CommonModule,CockpitComponent,ServerElementComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-The-Complete-Guide-standalone';
+  serverElements = [{type: 'server', name: 'Testserver', content: 'Just a test!'}];
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed!';
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0, 1);
+  }
 }
