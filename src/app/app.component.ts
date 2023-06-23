3a0d18559f96837e2ba70b1bf98260f78e53f81d
justine92415 <justine92415@gmail.com>
2023-06-23 13:28:15 +0800

    增加 路由設定

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { ServersComponent } from './servers/servers.component';
 import { UsersComponent } from './users/users.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports:[CommonModule,HomeComponent,ServersComponent,UsersComponent],
+  imports:[CommonModule,RouterOutlet,HomeComponent,ServersComponent,UsersComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { UsersComponent } from './users/users.component';
+import { ServerComponent } from './servers/server/server.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'users',component:UsersComponent},
+  {path:'servers',component:ServerComponent}
+];
