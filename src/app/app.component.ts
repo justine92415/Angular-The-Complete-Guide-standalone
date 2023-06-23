1d678d241c09f142a47f6b31afd2a820b9c2ee73
justine92415 <justine92415@gmail.com>
2023-06-23 13:31:45 +0800

    增加 RouterLink 設定

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { ServersComponent } from './servers/servers.component';
 import { UsersComponent } from './users/users.component';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone:true,
-  imports:[CommonModule,RouterOutlet,HomeComponent,ServersComponent,UsersComponent],
+  imports:[CommonModule,RouterOutlet,RouterLink,HomeComponent,ServersComponent,UsersComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
