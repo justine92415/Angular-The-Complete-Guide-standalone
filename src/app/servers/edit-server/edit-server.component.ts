0649d303463f1175df32da42d304229fd7e06b32
justine92415 <justine92415@gmail.com>
2023-06-23 15:32:00 +0800

    取得 queryParams 和 fragment

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -3,22 +3,30 @@ import { Component, OnInit, inject } from '@angular/core';
 import { ServersService } from '../servers.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
-  standalone:true,
-  imports:[CommonModule,FormsModule],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './edit-server.component.html',
-  styleUrls: ['./edit-server.component.css']
+  styleUrls: ['./edit-server.component.css'],
 })
 export class EditServerComponent implements OnInit {
-  server?: {id: number, name: string, status: string};
+  server?: { id: number; name: string; status: string };
   serverName = '';
   serverStatus = '';
 
   serversService = inject(ServersService);
 
+  route = inject(ActivatedRoute);
+
   ngOnInit() {
+    console.log(this.route.snapshot.queryParams);
+    console.log(this.route.snapshot.fragment);
+    this.route.queryParams.subscribe();
+    this.route.queryParams.subscribe();
+
     this.server = this.serversService.getServer(1);
     if (this.server) {
       this.serverName = this.server.name;
@@ -28,8 +36,10 @@ export class EditServerComponent implements OnInit {
 
   onUpdateServer() {
     if (this.server) {
-      this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+      this.serversService.updateServer(this.server.id, {
+        name: this.serverName,
+        status: this.serverStatus,
+      });
     }
   }
-
 }
