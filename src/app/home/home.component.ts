f08ea12db375a6c6d4f5a59e46bf56591767b894
justine92415 <justine92415@gmail.com>
2023-06-23 16:42:42 +0800

    實作 假的登入登出功能

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-home',
@@ -12,9 +13,21 @@ import { Router } from '@angular/router';
 export class HomeComponent implements OnInit {
   router = inject(Router);
 
+  authService = inject(AuthService);
+
   ngOnInit() {}
 
   onLoadServers(id: number) {
-    this.router.navigate(['/servers',id,'edit'],{queryParams:{allowEdit:'1'},fragment:'loading'});
+    this.router.navigate(['/servers', id, 'edit'], {
+      queryParams: { allowEdit: '1' },
+      fragment: 'loading',
+    });
+  }
+
+  onLogin() {
+    this.authService.login();
+  }
+  onLogout() {
+    this.authService.logout();
   }
 }
