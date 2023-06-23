d7e32b7b7389d9ef1610262db1ea116b8dba2d01
justine92415 <justine92415@gmail.com>
2023-06-23 14:43:52 +0800

    增加 user/:id 路由

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { ServersComponent } from './servers/servers.component';
+import { UserComponent } from './users/user/user.component';
 
 export const routes: Routes = [
   {path:'',component:HomeComponent},
   {path:'users',component:UsersComponent},
+  {path:'users/:id',component:UserComponent},
   {path:'servers',component:ServersComponent}
 ];
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -26,6 +26,6 @@ export class ServersComponent implements OnInit {
   }
 
   onReload() {
-    this.router.navigate(['servers'], { relativeTo: this.route });
+    // this.router.navigate(['/servers'], { relativeTo: this.route });
   }
 }
