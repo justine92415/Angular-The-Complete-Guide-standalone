eaf6be2e18fe63964cd5b25c820884ab7152fc3d
justine92415 <justine92415@gmail.com>
2023-06-24 11:39:53 +0800

    增加 子路由

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 
 export const routes: Routes = [
   {
@@ -11,6 +13,16 @@ export const routes: Routes = [
   {
     path: 'recipes',
     component: RecipesComponent,
+    children: [
+      {
+        path: '',
+        component: RecipeStartComponent
+      },
+      {
+        path: ':id',
+        component: RecipeDetailComponent
+      }
+    ]
   },
   {
     path: 'shopping-list',
